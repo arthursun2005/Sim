e58941203153b015cf1ed4b465162616969b6516
Arthur Sun <arthursun2005@gmail.com>
2019-02-25 18:41:23 +1100

    Initial Commit

---
+++ Sim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sim
+//
+//  Created by Arthur Sun on 2/25/19.
+//  Copyright © 2019 Arthur Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
